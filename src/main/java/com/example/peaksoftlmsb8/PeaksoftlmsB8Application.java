6de2f627952bfb70623d56dd50e92d9b5c973698
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-04-04 12:43:21 +0600

    first commit

---
+++ src/main/java/com/example/peaksoftlmsb8/PeaksoftlmsB8Application.java
@@ -0,0 +1,20 @@
+package com.example.peaksoftlmsb8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class PeaksoftlmsB8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeaksoftlmsB8Application.class, args);
+    }
+
+    @GetMapping
+    String introduction(){
+        return "introduction";
+    }
+}
