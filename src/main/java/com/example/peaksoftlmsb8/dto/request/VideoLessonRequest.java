e2eaf2a3b9b93acb6d7c9a1367220b719559da29
AbdirazakN <abdirazak.nooruzbai@gmail.com>
2023-04-24 11:41:18 +0600

    added video dto

---
+++ src/main/java/com/example/peaksoftlmsb8/dto/request/VideoLessonRequest.java
@@ -0,0 +1,7 @@
+package com.example.peaksoftlmsb8.dto.request;
+
+public class VideoLessonRequest {
+    private String name;
+    private String description;
+    private String videoLink;
+}
---
+++ src/main/java/com/example/peaksoftlmsb8/dto/response/VideoLessonResponse.java
@@ -0,0 +1,7 @@
+package com.example.peaksoftlmsb8.dto.response;
+
+public class VideoLessonResponse {
+    private String name;
+    private String description;
+    private String videoLink;
+}
