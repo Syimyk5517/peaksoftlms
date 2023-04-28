0a9734a646c1004cd45f5633f432c8532b2f8f10
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-04-28 17:48:38 +0600

    refactor data init and Student crud

--- src/main/java/com/example/peaksoftlmsb8/api/StudentApi.java
+++ src/main/java/com/example/peaksoftlmsb8/api/StudentApi.java
@@ -21,7 +21,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
-
 @Tag(name = "Students")
 public class StudentApi {
     private final StudentService studentService;
