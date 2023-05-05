447bc9f7bd57bab5b07cd9ad021e590a4d4e0472
bekzadsarybaev <bekzat97isakov>
2023-05-05 11:27:48 +0600

    refactor course

--- src/main/java/com/example/peaksoftlmsb8/service/impl/CourseServiceImpl.java
+++ src/main/java/com/example/peaksoftlmsb8/service/impl/CourseServiceImpl.java
@@ -18,14 +18,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
