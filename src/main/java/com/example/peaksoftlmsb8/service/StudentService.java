30fc54c8792237981779c32402ddd2b08f00c3ad
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-05-23 22:49:22 +0600

    refactor_instructor_method_delete

--- src/main/java/com/example/peaksoftlmsb8/service/StudentService.java
+++ src/main/java/com/example/peaksoftlmsb8/service/StudentService.java
@@ -1,10 +1,9 @@
 package com.example.peaksoftlmsb8.service;
 
-import com.example.peaksoftlmsb8.dto.request.student.StudentRequest;
+import com.example.peaksoftlmsb8.dto.request.StudentRequest;
 import com.example.peaksoftlmsb8.dto.response.SimpleResponse;
 import com.example.peaksoftlmsb8.dto.response.student.StudentPaginationResponse;
 import com.example.peaksoftlmsb8.dto.response.student.StudentResponse;
-import com.example.peaksoftlmsb8.dto.response.student.StudentResponseForAdmin;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
