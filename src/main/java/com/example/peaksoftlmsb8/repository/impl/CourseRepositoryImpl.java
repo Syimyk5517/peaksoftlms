ae07fb1b103f527fd2494a558808856947aa45c5
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-06-21 14:04:07 +0600

    refactor course delete

--- src/main/java/com/example/peaksoftlmsb8/repository/impl/CourseRepositoryImpl.java
+++ src/main/java/com/example/peaksoftlmsb8/repository/impl/CourseRepositoryImpl.java
@@ -16,36 +16,40 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CourseRepositoryImpl implements CourseRepo {
     private final JdbcTemplate jdbcTemplate;
+
     @Override
     public CoursePaginationResponse getAllCourses(User user, int size, int page) {
-        int offset = size * page;
+        int offset = (page - 1) * size;
         if (user.getStudent().getGroup() == null) {
             throw new NotFoundException("В это студенте пока что курсы нету");
         } else {
             Group group = user.getStudent().getGroup();
             String sql = """
-                    select c.id as course_id,
-                           c.name as course_name,
-                           c.description as course_description,
-                           c.created_at as course_created_at,
-                           c.finish_date as course_finish_date
-                    from courses c join groups_courses gc on c.id = gc.courses_id where gc.group_id =:? limit ? offset ?;
+                    SELECT c.id AS course_id,
+                           c.name AS course_name,
+                           c.description AS course_description,
+                           c.created_at AS course_created_at,
+                           c.finish_date AS course_finish_date
+                    FROM courses c
+                    JOIN courses_groups gc ON c.id = gc.courses_id
+                    WHERE gc.groups_id = ?
+                    LIMIT ? OFFSET ?;
                     """;
-            List<CourseResponse> courseResponses = jdbcTemplate.query(sql, (resultSet, i) -> {
+            List<CourseResponse> courseResponses = jdbcTemplate.query(sql, (resultSet, rowNum) -> {
                 CourseResponse courseResponse = new CourseResponse();
                 courseResponse.setId(resultSet.getLong("course_id"));
                 courseResponse.setName(resultSet.getString("course_name"));
                 courseResponse.setDescription(resultSet.getString("course_description"));
-                courseResponse.setCreatedAt(resultSet.getDate("course_create_date").toLocalDate());
+                courseResponse.setCreatedAt(resultSet.getDate("course_created_at").toLocalDate());
                 courseResponse.setFinishDate(resultSet.getDate("course_finish_date").toLocalDate());
                 return courseResponse;
-
             }, group.getId(), size, offset);
-            return CoursePaginationResponse.builder().
-                    courseResponses(courseResponses)
+
+            return CoursePaginationResponse.builder()
+                    .courseResponses(courseResponses)
                     .currentPage(page)
-                    .pageSize(size).
-                    build();
+                    .pageSize(size)
+                    .build();
         }
     }
 }
