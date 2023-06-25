c8f9361dbe5043a707d08ad9856cac2135327807
Syimykjumabekov <syjmykzumabekuulu@gmail.com>
2023-06-25 12:53:05 +0600

    refactor reset password

--- src/main/java/com/example/peaksoftlmsb8/dto/request/authentication/PasswordRequest.java
+++ src/main/java/com/example/peaksoftlmsb8/dto/request/authentication/PasswordRequest.java
@@ -14,6 +14,5 @@ public class PasswordRequest {
     private Long id;
     @NotBlank(message = "Пароль не должна быть пустым.")
     @NotNull(message = "Пароль не должна быть пустым.")
-    @PasswordValid
     private String password;
 }
--- src/main/java/com/example/peaksoftlmsb8/validation/password/PasswordValidator.java
+++ src/main/java/com/example/peaksoftlmsb8/validation/password/PasswordValidator.java
@@ -3,41 +3,37 @@ package com.example.peaksoftlmsb8.validation.password;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class PasswordValidator  implements ConstraintValidator<PasswordValid, String> {
-    @Override
-    public boolean isValid(String password, ConstraintValidatorContext context) {
-        if (password == null || password.length() < 4) {
+public class PasswordValidator implements ConstraintValidator<PasswordValid, String> {
+    public boolean isValid(String password, int minLength, boolean requireLetter, boolean requireSymbol, boolean requireDigit) {
+        if (password == null || password.length() < minLength) {
             return false;
         }
 
-        boolean hasEnglishLetter = false;
-        boolean hasEnglishUppercase = false;
-        boolean hasRussianLetter = false;
-        boolean hasRussianUppercase = false;
+        boolean hasLetter = false;
         boolean hasSymbol = false;
+        boolean hasDigit = false;
 
         for (char c : password.toCharArray()) {
             if (Character.isLetter(c)) {
-                if (Character.UnicodeBlock.of(c) == Character.UnicodeBlock.CYRILLIC) {
-                    hasRussianLetter = true;
-                    if (Character.isUpperCase(c)) {
-                        hasRussianUppercase = true;
-                    }
-                } else {
-                    hasEnglishLetter = true;
-                    if (Character.isUpperCase(c)) {
-                        hasEnglishUppercase = true;
-                    }
+                if (Character.UnicodeBlock.of(c) == Character.UnicodeBlock.CYRILLIC || Character.isLetter(c)) {
+                    hasLetter = true;
                 }
+            } else if (Character.isDigit(c)) {
+                hasDigit = true;
             } else {
                 hasSymbol = true;
             }
-
-            if (hasEnglishLetter && hasEnglishUppercase && hasRussianLetter && hasRussianUppercase && hasSymbol) {
-                return true;
-            }
         }
 
+        boolean letterRequirementMet = !requireLetter || hasLetter;
+        boolean symbolRequirementMet = !requireSymbol || hasSymbol;
+        boolean digitRequirementMet = !requireDigit || hasDigit;
+
+        return letterRequirementMet && symbolRequirementMet && digitRequirementMet;
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
         return false;
     }
 }
